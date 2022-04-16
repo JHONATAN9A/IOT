b5358ce76ecf8c1f57e660f01ace588e140fd7e3
JONATHAN9A <calderonjonatan99@gmail.com>
2022-04-16 12:25:49 -0500

    icons

--- web/dist/src.e31bb0bc.js
+++ web/dist/src.e31bb0bc.js
@@ -25142,7 +25142,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55603" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51419" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
